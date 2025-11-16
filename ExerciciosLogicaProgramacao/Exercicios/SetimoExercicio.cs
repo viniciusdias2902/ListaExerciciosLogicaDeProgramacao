5db6d5361569f680885f1d1fedda70f1a561122f
Vinícius Dias <vyvy292004@gmail.com>
2025-11-16 03:13:32 -0300

    Finalização do sétimo exercício

--- ExerciciosLogicaProgramacao/Exercicios/SetimoExercicio.cs
+++ ExerciciosLogicaProgramacao/Exercicios/SetimoExercicio.cs
@@ -11,7 +11,18 @@ namespace ExerciciosLogicaProgramacao.Exercicios
         public override string Enunciado { get; } = "Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO.";
         public override void Resolucao()
         {
-            
+            bool valorBooleano1 = bool.Parse(Console.ReadLine());
+            bool valorBooleano2 = bool.Parse(Console.ReadLine());
+            if (valorBooleano1 && valorBooleano2)
+            {
+                Console.WriteLine("Ambos são verdadeiros");
+            }else if (!valorBooleano1 && !valorBooleano2)
+            {
+                Console.WriteLine("Ambos são falsos");
+            }else
+            {
+                Console.WriteLine("Os dois valores são diferentes");
+            }
         }
     }
 }
