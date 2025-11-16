a72114bc5e2719d97128534a0b336b68cc65bc1f
Vinícius Dias <vyvy292004@gmail.com>
2025-11-16 05:03:01 -0300

    Adiciona uma interface para questão

--- ExerciciosLogicaProgramacao/Exercicios/PrimeiraQuestao.cs
+++ ExerciciosLogicaProgramacao/Exercicios/PrimeiraQuestao.cs
@@ -6,6 +6,9 @@ namespace ExerciciosLogicaProgramacao.Exercicios
 {
     class PrimeiraQuestao
     {
-                 
+        private string enunciado = "Faça um algoritmo que leia os valores de A, B, C" +
+            " e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.";
+        
+
     }
 }
