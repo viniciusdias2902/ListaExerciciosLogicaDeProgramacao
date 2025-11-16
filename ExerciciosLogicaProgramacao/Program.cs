90949b8f03dec1c9ed270d848278ff36eea94d30
Vinícius Dias <vyvy292004@gmail.com>
2025-11-16 01:29:16 -0300

    Instalação do pacote spectre console

--- ExerciciosLogicaProgramacao/Program.cs
+++ ExerciciosLogicaProgramacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ExerciciosLogicaProgramacao.Exercicios;
+using Spectre.Console;
 
 namespace ExercicioLogicaProgramacao;
 
@@ -6,12 +7,15 @@ class Program
 {
     public static void Main(String[] args)
     {
-       var questoesResolvidas = new List<Questao>();
-       questoesResolvidas.Add(new PrimeiraQuestao());
-       foreach(var questao in questoesResolvidas)
-       {
-            Console.WriteLine(questao.Enunciado);
-            questao.Resolucao();
-       }
+        var questoesResolvidas = new List<Questao>();
+        questoesResolvidas.Add(new PrimeiraQuestao());
+        questoesResolvidas.Add(new SegundaQuestao());
+        FigletText apresentacao = new("Questões Resolvidas");
+        apresentacao.Centered();
+        apresentacao.Color = Color.Red;
+        foreach(var questao in questoesResolvidas)
+        {
+            AnsiConsole.WriteLine(questao.Enunciado);
+        }
     }
 }
