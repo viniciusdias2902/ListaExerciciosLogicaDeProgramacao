e4f8eb66bc81e84b39b52dbe834facd10ac6abf1
Vinícius Dias <vyvy292004@gmail.com>
2025-11-16 03:08:13 -0300

    Finalização do sexto exercício

--- ExerciciosLogicaProgramacao/Exercicios/QuintoExercicio.cs
+++ ExerciciosLogicaProgramacao/Exercicios/QuintoExercicio.cs
@@ -11,7 +11,9 @@ namespace ExerciciosLogicaProgramacao.Exercicios
         public override string Enunciado { get; } = " Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.";
         public override void Resolucao()
         {
-           
+            double valor = double.Parse(Console.ReadLine());
+            double valorReajustado = valor + (valor * 0.05);
+            Console.WriteLine(valorReajustado);
         }
     }
 }
