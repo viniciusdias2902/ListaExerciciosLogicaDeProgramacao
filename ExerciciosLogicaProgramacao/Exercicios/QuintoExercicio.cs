3512306074f67c9814ef1a10bd34e5d30db8deb6
Vinícius Dias <vyvy292004@gmail.com>
2025-11-16 02:23:10 -0300

    Resolução do quinto exercício

---
+++ ExerciciosLogicaProgramacao/Exercicios/QuintoExercicio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLogicaProgramacao.Exercicios
+{
+    internal class QuintoExercicio : Exercicio
+    {
+        public override string Enunciado { get; } = "Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).";
+
+        public override void Resolucao()
+        {
+            double salarioMinimo = 1293.20;
+            double salarioUsuario = double.Parse(Console.ReadLine());
+            double quantidadeSalariosMinimos = salarioUsuario / salarioMinimo;
+            Console.WriteLine($"O usuário ganha {quantidadeSalariosMinimos} vezes o salário mínimo");
+
+        }
+    }
+}
--- ExerciciosLogicaProgramacao/obj/Debug/net8.0/ExerciciosLogicaProgramacao.AssemblyInfo.cs
+++ ExerciciosLogicaProgramacao/obj/Debug/net8.0/ExerciciosLogicaProgramacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciciosLogicaProgramacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f6537dcb9373cd8eeae5ac93d74437c350efed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040c8027cc434516387ae86d3ac2f7faa75e36e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciciosLogicaProgramacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciciosLogicaProgramacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
